880eb51d39b8bb2725eede1bd38801f39d624de6
Tyler Duran <tymduran@gmail.com>
2022-03-28 23:42:29 -0700

    Initial Commit

---
+++ script.js
@@ -0,0 +1,48 @@
+/***********************************************/
+// feed 1
+
+
+var count = 9;
+var countElement = document.querySelector("#count");
+
+console.log(countElement);
+
+
+function add1() {
+    count++;
+    countElement.innerText = count + "Likes(s)";
+    console.log(count);
+}
+
+
+/***********************************************/
+// feed 2
+
+
+var count1 = 12;
+var count1Element = document.querySelector("#count1");
+
+console.log(count1Element);
+
+
+function add2() {
+    count1++;
+    count1Element.innerText = count1 + "Likes(s)";
+    console.log(count1);
+}
+
+/***********************************************/
+// feed 3
+
+
+var count2 = 9;
+var count2Element = document.querySelector("#count2");
+
+console.log(count2Element);
+
+
+function add3() {
+    count2++;
+    count2Element.innerText = count2 + "Likes(s)";
+    console.log(count2);
+}
